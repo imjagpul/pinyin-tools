20daa0d25111f62c72e94d2e1cc1f900fb681f0e
Stepan Srubar <imjagpul@gmail.com>
2015-04-28 09:42:06 +0200

    Icons for system index.

--- pt/protected/views/system/_view.php
+++ pt/protected/views/system/_view.php
@@ -9,19 +9,31 @@
 	<?php echo CHtml::link(CHtml::encode($data->name), array('system/view', 'id'=>$data->id)); ?>
 	(by <?php echo $data->masterUser->username ?>)
 	
-	<?php 
-	if($data->isWriteable()) {
-		echo '<span class="editlink">['.CHtml::link("Edit", array('system/update', 'id'=>$data->id)).']</span>';		
-	}
-	//echo "[Browse]";
-	//echo "[Favorite]"; //perhaps move these two to the description 
-	//echo "[Hide]";
+	<span style="float: right">
 	
+	<?php 
+		echo '<span class="editlink">';
+	$iconsPath=Yii::app()->request->baseUrl.'/images/icons/';
+	if($data->isWriteable()) {
+		echo CHtml::link(CHtml::image($iconsPath.'primary.png', "").'Primary', array('system/update', 'id'=>$data->id));
+		echo "&nbsp;";
+		echo CHtml::link(CHtml::image($iconsPath.'edit.png', "").'Edit', array('system/update', 'id'=>$data->id));
+	} else {
+		echo "&nbsp;";
+		echo CHtml::link(CHtml::image($iconsPath.'favorite.png', "").'Favorite', array('system/update', 'id'=>$data->id));
+		echo "&nbsp;";
+		echo CHtml::link(CHtml::image($iconsPath.'hide.png', "").'Hide', array('system/update', 'id'=>$data->id));
+	}
+		echo "&nbsp;";
+		echo CHtml::link(CHtml::image($iconsPath.'browse.png', "").'Browse', array('system/update', 'id'=>$data->id));
+	echo '</span>';		
 	?>
+	</span>
+	
 	<br />
 	<?php 
 	$l=$data->targetLanguageData; if($l!==NULL) echo 'for <b>'.$l->text.'</b>'; 
-	$l=$data->languageData; if($l!==NULL) echo '('.$l->text.')'; 
+	$l=$data->languageData; if($l!==NULL) echo ' ('.$l->text.')'; 
 	?>
 	<br />
 	<?php echo CHtml::encode($data->shortenedDescription); ?>
