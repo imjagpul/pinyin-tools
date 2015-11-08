b008dfba1792cad4fa672feb323eb90db7f76b0a
Stepan Srubar <imjagpul@gmail.com>
2015-11-08 15:06:15 +0100

    three steps tutorial on homepage - images

--- pt/protected/views/site/index.php
+++ pt/protected/views/site/index.php
@@ -20,20 +20,31 @@
 Here is the solution.
 </h3>
 
-<p>(1） (2） (3）</p>
+<div class="figblock">
+&nbsp;
+<div class="figure"><p><img src="../images/homepage/01.png"  /> 
+</p><p>(1）</p></div>
+<div class="figure"><p><img src="../images/homepage/02.png"  /> 
+</p><p>(2）</p></div>
+<div class="figure"><p><img src="../images/homepage/03.png"  /> 
+</p><p>(3）</p></div>
+&nbsp;
+</div>
 
+<p>
 <ul>
 	<li>If you do not know why mnemonics should be of any use for learning Chinese characters, read [About mnemonics].</li>
 	<li>If you are using mnemonics already, or plan to - take a look at the [Demo page] to see how this site might help you.</li>
 </ul>
+</p>
 
 <p>TOC (Manual)
-
+</p>
 
 
 <p>(Beta-test image)
 Note this site has just launched, so in case you encounter any errors, have suggestions how this site could be more useful, 
 please let me know. (Also thanks for correcting my English, I am not a native speaker.)
+</p>
 
-
-<p>Footer
+<p>Footer</p>
