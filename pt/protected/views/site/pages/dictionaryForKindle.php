9ad3b109aebba99baaeedc4be54bee4423a539f2
Stepan Srubar <imjagpul@gmail.com>
2015-12-02 17:34:48 +0100

    manual entry stubs

---
+++ pt/protected/views/site/pages/dictionaryForKindle.php
@@ -0,0 +1,15 @@
+<?php /* NEED images! */ ?>
+Note this site works also as a (kind of) dictionary for e-book readers.
+<p>
+Simply run your text through the <a href="<?php echo Yii::app()->createUrl("/annotator/input", array("modeID"=>3)); ?>">annotator</a> and create a MOBI or EPUB file. 
+<p>
+You can then click on any character in the text and see the translation. If the character is the beginning of a word,
+you will see the translation of the word as well.
+<p>
+
+<img alt="" src="">Chinese-English dictionary on Kindle 
+
+
+(Or, if you are using the mnemonics you can add them as well:)
+
+<img alt="" src="">Chinese-German dictionary on Kindle with mnemonics added 
---
+++ pt/protected/views/site/pages/useYourReader.php
@@ -0,0 +1,17 @@
+There is a custom loved by the children of our country.
+...
+
+At the moment I received an e-book reader ...
+
+did not yet suspect how useful it will prove for the language learning.
+
+
+Internet has many libraries...
+
+
+*foreign books
+*eyes tired
+*dictionary
+*swapable
+*notes
+*transportable
